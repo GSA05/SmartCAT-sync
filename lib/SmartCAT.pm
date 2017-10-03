dc840afd7a24612e2ff74829c6c9ae82c340f595
Sergey Goremykin <GSA05@mail.ru>
2017-10-03 17:55:34 +0300

    feat(#1): Authorization key

--- lib/SmartCAT.pm
+++ lib/SmartCAT.pm
@@ -1,3 +1,16 @@
 package SmartCAT;
+
 use App::Cmd::Setup -app;
+use Class::Load;
+#use Data::Dumper;
+
+sub getAuthKey {
+    my ($self, $token_id, $token) = @_;
+
+    Class::Load::load_class('MIME::Base64');
+    my $key = MIME::Base64::encode_base64("$token_id:$token");
+
+    return $key;
+}
+
 1;
